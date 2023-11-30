e0423f31d0919afff155b2133175c0334b75fb6a
GokuWorks <sayon7511@gmail.com>
2023-11-29 23:47:53 -0500

    WIP VideoDir.cpp add dir name fn

--- src/VideoDir.cpp
+++ src/VideoDir.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <filesystem>
+
+using std::vector, std::string;
+
+typedef vector<string> vStr_t;
+
+static void getDir(vStr_t &v, const string &p) {
+	for (const auto &entry : std::filesystem::directory_iterator(p)) {
+		v.push_back(entry.path().filename().string());
+	}
+}
 
 int main() {
+	const string ROOT = "Y:/Media/Anime";
+	vStr_t top;
 
 	return 0;
 }
