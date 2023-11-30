5639829ca756e8c7ca3933dc3efc8806b88870eb
GokuWorks <sayon7511@gmail.com>
2023-11-29 23:57:31 -0500

    WIP VideoDir.cpp add print fn

--- src/VideoDir.cpp
+++ src/VideoDir.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <filesystem>
 
-using std::vector, std::string, std::cout;
+using std::vector, std::string, std::cout, std::endl;
 
 typedef vector<string> vStr_t;
 
@@ -9,6 +9,11 @@ static void getDir(vStr_t &v, const string &p) {
 	for (const auto &entry : std::filesystem::directory_iterator(p)) {
 		v.push_back(entry.path().filename().string());
 	}
+}
+
+static void printEntry(const string& s, const string& topS) {
+	cout << "\033[31m" << s << "\033[33m" << " from: "
+		<< topS << "\033[0m" << endl;
 }
 
 int main() {
@@ -21,7 +26,7 @@ int main() {
 		string topS(entry.data());
 
 		if ((!entry.contains("(") || !entry.contains(")")) && !entry.contains("#Download")) {
-			cout << entry;
+			printEntry(entry, topS);
 			continue;
 		}
 	}
